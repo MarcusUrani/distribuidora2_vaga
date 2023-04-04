48798a05c6ff995d78517fc498a08f9fad121fad
Marcus <marcus.urani20@gmail.com>
2023-04-04 09:36:44 -0300

    Implementada função que calcula o total das distribuidoras

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const branches = [
-  { id: "1", name: "SP", invoicing: 67836.43 },
-  { id: "2", name: "RJ", invoicing: 36678.66 },
-  { id: "3", name: "MG", invoicing: 29229.88 },
-  { id: "4", name: "ES", invoicing: 27165.48 },
-  { id: "5", name: "Outros", invoicing: 19849.53 },
+  { id: "1", name: "SP", invoicing: 67836.43, percents: 0 },
+  { id: "2", name: "RJ", invoicing: 36678.66, percents: 0 },
+  { id: "3", name: "MG", invoicing: 29229.88, percents: 0 },
+  { id: "4", name: "ES", invoicing: 27165.48, percents: 0 },
+  { id: "5", name: "Outros", invoicing: 19849.53, percents: 0 },
 ];
 
 function getTotalValue(data) {
@@ -11,7 +11,9 @@ function getTotalValue(data) {
   for (let i = 0; i < data.length; i++) {
     totalValue += data[i].invoicing;
   }
-  console.log(totalValue);
+  calcPercents(totalValue, data);
 }
+
+function calcPercents(value, data) {}
 
 getTotalValue(branches);
