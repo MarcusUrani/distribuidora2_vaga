c1a7541ab9901fc014d4b0a583151d75c824134a
Marcus <marcus.urani20@gmail.com>
2023-04-04 10:19:19 -0300

    Implementada função que adiciona os resultados a tela

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const branches = [
   { id: "5", name: "Outros", invoicing: 19849.53, percents: 0 },
 ];
 
+const resultSection = document.querySelector(".main__container__results");
+
 function getTotalValue(data) {
   let totalValue = 0;
   for (let i = 0; i < data.length; i++) {
@@ -18,6 +20,24 @@ function calcPercents(value, data) {
   for (let i = 0; i < data.length; i++) {
     data[i].percents = (data[i].invoicing / value) * 100;
   }
+  showDataOnScreen();
+}
+
+function showDataOnScreen() {
+  resultSection.innerHTML = branches
+    .map(
+      (item) =>
+        `<article key="${item.id}" class=".main__container__results_item">
+          <h2 class=".main__container__results__item__title">Resultado da distribuidora: ${
+            item.name
+          }</h2>
+          <p class=".main__container__results__item__percents">
+            ${item.percents.toFixed(2)} %
+          </p>
+          </article>
+         `
+    )
+    .join("");
 }
 
 getTotalValue(branches);
